2a37dcc866c4ee15920bdf099bd886b1dc31002e
napiiis <72500111+napiiis@users.noreply.github.com>
2023-05-17 15:21:48 +0700

    Appium Test

---
+++ src/Test.java
@@ -0,0 +1,25 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+import java.net.MalformedURLException;
+
+public class Test extends AppiumTest {
+    public static void main(String[] args) throws MalformedURLException {
+        AndroidDriver<AndroidElement> driver = capabilities();
+        
+        String value = "0";
+
+        driver.findElementById("com.example.volumebalok:id/edt_length").sendKeys("1");
+        driver.findElementById("com.example.volumebalok:id/edt_width").sendKeys("2");
+        driver.findElementById("com.example.volumebalok:id/edt_height").sendKeys("3");
+        driver.findElementById("com.example.volumebalok:id/btn_calculate").click();
+        
+        for (int i = 1; i <= 2; i++) {
+            driver.findElementById("com.example.volumebalok:id/edt_length").sendKeys(value);
+            driver.findElementById("com.example.volumebalok:id/edt_width").sendKeys(value);
+            driver.findElementById("com.example.volumebalok:id/edt_height").sendKeys(value);
+            driver.findElementById("com.example.volumebalok:id/btn_calculate").click();
+        }
+
+    }
+}
